1eded9a2e5b088cd47dad188a75009ce7a03c805
InioluwaR <inioluwaroberts@gmail.com>
2022-09-08 11:46:49 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,8 +18,8 @@ int main(void)
 
 {
 
-		puts("Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle\n");
 
-			return (0);
+			return 0;
 
 }
