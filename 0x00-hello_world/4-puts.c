bd4eb754870836e3a57e08975e76c442b91f6df3
InioluwaR <inioluwaroberts@gmail.com>
2022-09-08 11:58:41 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  *    main - Entry point
  *
  *
@@ -17,5 +16,5 @@ int main(void)
 
 {
 		puts("Programming is like building a multilingual puzzle\n");
-		return 0;
+		return (0);
 }
