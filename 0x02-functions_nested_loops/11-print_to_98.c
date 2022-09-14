b717472ad192c30f4f9c116689c6d175e5d91008
InioluwaR <inioluwaroberts@gmail.com>
2022-09-13 19:31:00 -0700

    check abeg

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,6 +16,16 @@ void print_to_98(int n)
 				printf(", ");
 			}
 		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
-	printf("\n");
+	printf('\n');
 }
