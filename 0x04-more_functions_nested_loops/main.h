1b7785320af813926075394b7846a68a727e55b9
InioluwaR <inioluwaroberts@gmail.com>
2022-09-15 08:55:04 -0700

    upper commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - determines uppercase or not
+ * @c: character to determine
+ * Return: true or false
+ */
+int _isupper(int c);
+/**
+ * _isdigit - checks if it is digit
+ * @c: character to check
+ * Return: returns boolean
+ */
