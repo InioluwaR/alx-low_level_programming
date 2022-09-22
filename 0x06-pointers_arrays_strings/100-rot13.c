9bed1b8e91b187ec568f566fb3cb4d79279def84
InioluwaR <inioluwaroberts@gmail.com>
2022-09-22 14:21:13 -0700

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encrypts code
+ * @s: string to encrypt
+ * Return: char value
+ */
+
+char *rot13(char *s)
+{
+	char c1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char c2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c1[j] != '\0'; j++)
+		{
+			if (str[i] == c1[j])
+			{
+				str[i] = c2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
