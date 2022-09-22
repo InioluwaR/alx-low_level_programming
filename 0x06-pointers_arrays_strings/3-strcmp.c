3b1f896cf9a58fd4b21dbb742d15f80efffb94e2
InioluwaR <inioluwaroberts@gmail.com>
2022-09-22 13:05:19 -0700

    cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - This funtion compares two strings
+ * @s1: first param
+ * @s2: second param
+ *
+ * Return: returns the difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int diff;
+
+	diff = strcmp(s1, s2);
+	return (diff);
+}
