054b46285d530a91e1eda38b29595fe5205d37b5
InioluwaR <inioluwaroberts@gmail.com>
2022-10-06 15:49:56 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -49,6 +49,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	for (index = 0; index < old_size && index < new_size; index++)
 		filler[index] = *ptr_copy++;
 
-	fre(ptr);
+	free(ptr);
 	return (mem);
 }
