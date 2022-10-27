e8f5baa74ab1a004b7c5ba1c6fbc8bf8222b01fa
InioluwaR <inioluwaroberts@gmail.com>
2022-10-26 19:21:48 -0700

    added

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,6 +26,7 @@ typedef struct listint_s
 } listint_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
