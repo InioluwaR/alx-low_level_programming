4f14c8f40e930f7396d3cf454e2e282a1104ab0c
InioluwaR <inioluwaroberts@gmail.com>
2022-10-31 17:32:40 -0700

    create_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,13 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
+
 		wrote = write(fd, text_content, len);
 		if (wrote == -1)
 			return (-1);
